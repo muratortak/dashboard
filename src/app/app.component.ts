f784fa87adb10acf14d8ac2e2b3b0203902c8e12
murat ortak <murat.ortak35@hotmail.com>
2020-02-25 18:22:46 -0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dashboard-client';
+  title = 'Dashboard Client';
+  subtitle ='Dashboard Angular-Client';
 }
